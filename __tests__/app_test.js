3ce1fbe4930d862f45f7b0cf9272d97e91f66227
Arya Sukma Wijaya <aryasukma.work@gmail.com>
2021-02-26 22:14:39 +0700

    Add a new test

--- __tests__/app_test.js
+++ __tests__/app_test.js
@@ -1,7 +1,15 @@
 const request = require("supertest");
 const app = require("../app");
 
-test("It should response the GET method", () => {
+test("Test / route", () => {
+	return request(app)
+		.get("/")
+		.then(response => {
+			expect(response.text).toBe("Hello World!");
+		});
+});
+
+test("Test /arya route", () => {
 	return request(app)
 		.get("/arya")
 		.then(response => {
